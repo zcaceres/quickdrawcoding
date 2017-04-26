f9122152fe46136a5da60405544d37fb10855da9
zc <zachcaceres@gmail.com>
2017-04-26 11:47:06 -0400

    initial commit

---
+++ Assets/GitScraper.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var www;
+function Start () {
+  www : WWW = new WWW('https://github.com/zcaceres/react-desktop');
+  
+
+}
+
+function Update () {
+//	if (www.isDone) {
+//		Debug.Log(www.text);
+//	}
+}
