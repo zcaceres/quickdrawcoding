6bf2c3be9b52706b45113a2dcc1893f4e0656af5
zc <zachcaceres@gmail.com>
2017-04-28 21:51:32 -0400

    feat: main menu camera lerp

--- Assets/SFX/CameraLerpController.cs
+++ Assets/SFX/CameraLerpController.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class CameraLerpController : MonoBehaviour {
 	public Transform lerpStart;
 	public Transform lerpEnd;
-	// Use this for initialization
-	void Start () {
+	private bool atEnd;
 
+	void Start () {
+		atEnd = false;
 	}
+
+
 
 	// Update is called once per frame
 	void Update () {
-		var targetPosition = lerpEnd.position;
+		var targetPosition = atEnd ? lerpStart.position : lerpEnd.position;
+		if (Mathf.Abs(transform.position.x - lerpEnd.position.x) < .1f) {
+			atEnd = true;
+		} else if (Mathf.Abs(transform.position.x - lerpStart.position.x) < .1f) {
+			atEnd = false;
+		}
 		transform.position =
 			Vector3.Lerp(transform.position,
 				targetPosition,
-				.1f*Time.deltaTime);
+				.2f*Time.deltaTime);
 	}
 }
