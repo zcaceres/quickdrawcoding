2b69170c0eb01613e450ba3392e2a2bae54c6e50
Zach Caceres <zachcaceres@gmail.com>
2017-07-03 19:40:49 -0400

    feat: add ability to toggle language menu from main menu

---
+++ Assets/Utils/LanguageManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageManager : MonoBehaviour {
+	private GameObject DifficultyButtons;
+
+	void ShowDifficultyButtons()
+	{
+		DifficultyButtons.SetActive(true);
+	}
+
+	void HideDifficultyButtons()
+	{
+		DifficultyButtons.SetActive(true);
+	}
+
+}
---
+++ Assets/Utils/StartButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour {
+	private GameObject LanguageButtons;
+	private GameObject Title;
+	private GameObject MainMenu;
+
+	void Awake()
+	{
+		LanguageButtons = gameObject.transform.Find("MenuPanel/LanguageButtons").gameObject;
+		Title = gameObject.transform.Find("MenuPanel/Title").gameObject;
+		MainMenu = gameObject.transform.Find("MenuPanel/MenuButtons").gameObject;
+	}
+
+	public void ShowLanguageMenu()
+	{
+		LanguageButtons.SetActive(true);
+		HideMainMenu();
+	}
+
+	void HideLanguageMenu()
+	{
+		LanguageButtons.SetActive(false);
+		ShowMainMenu();
+	}
+
+	void ShowMainMenu()
+	{
+		Title.SetActive(true);
+		MainMenu.SetActive(true);
+	}
+
+	void HideMainMenu()
+	{
+		Title.SetActive(false);
+		MainMenu.SetActive(false);
+	}
+
+	void SetLanguagePreference(string languagePref)
+	{
+
+	}
+
+
+}
